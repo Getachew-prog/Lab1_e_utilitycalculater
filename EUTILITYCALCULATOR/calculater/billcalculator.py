0253be7d3907b69326776d73d990a9f13c30b62c
Getachew-prog <getachewshifera27@gmail.com>
2025-12-04 08:23:25 -0400

    service charge modification

--- EUTILITYCALCULATOR/calculater/billcalculator.py
+++ EUTILITYCALCULATOR/calculater/billcalculator.py
@@ -1,22 +1,29 @@
 
 def calculate_bill(total_kwh):
     if total_kwh <= 50:
+        service_charge=10
         bill_amount=total_kwh * 0.2730
     elif total_kwh <= 100:
          service_charge=42.00
          bill_amount=total_kwh * 0.7670
     elif total_kwh <= 200:
+         service_charge=42.00
          bill_amount=total_kwh * 1.6200
     elif total_kwh <= 300:
+         service_charge=42.00
          bill_amount=total_kwh * 2.0000
     elif total_kwh <= 400:
+         service_charge=42.00
          bill_amount=total_kwh * 2.2000
     elif total_kwh <= 500:
+         service_charge=42.00
          bill_amount=total_kwh * 2.4050
     elif total_kwh > 500:
+         service_charge=42.00
          bill_amount=total_kwh * 2.4813
     else:
-        bill_amount=0   
+        bill_amount=0
+    if bill_amount   
     total_bill = bill_amount + service_charge
 
     print(f"Energy Charge: {bill_amount} ETB")
